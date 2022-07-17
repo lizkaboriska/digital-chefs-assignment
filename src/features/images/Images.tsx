30ba3c3093a4c2cda373208a3150d832c3c8d9bb
Elizaveta Borisova <lizkaboriska@gmail.com>
2022-07-17 16:38:03 +0200

    Open image in modal onClick

--- src/features/images/Images.tsx
+++ src/features/images/Images.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 import TextField from '@mui/material/TextField';
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
@@ -11,13 +9,13 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppDispatch } from '../../app/hooks';
 import {
-  selectImages,
   initialLoad,
   search
 } from './imagesSlice';
 import { Color, OrderBy, Orientation, SearchImages } from './types';
+import { ImagesList } from './components/ImagesList';
 
 const initialSearch: SearchImages = {
   query: '',
@@ -28,9 +26,7 @@ const initialSearch: SearchImages = {
 };
 
 export function Images() {
-  const images = useAppSelector(selectImages);
   const dispatch = useAppDispatch();
-
   const [searchParams, setSearchParams] = useState<SearchImages>(initialSearch);
 
   useEffect(() => {
@@ -63,106 +59,95 @@ export function Images() {
   return (
     <>
       <Container sx={{p: 2}} maxWidth="md">
-      <div>
-        <TextField
-          id="search-bar"
-          className="text"
-          label="Search"
-          value={searchParams.query || ''}
-          onChange={handleChangeQuery}
-          variant="outlined"
-          placeholder="Search..."
-          size="small"
-        />
-
-        <FormControl sx={{minWidth: 120 }}>
-          <InputLabel id="color-label" size='small'>Color</InputLabel>
-          <Select
-            labelId='color-label'
-            value={searchParams.color || ''}
-            label="Color"
+        <div>
+          <TextField
+            id="search-bar"
+            className="text"
+            label="Search"
+            value={searchParams.query || ''}
+            onChange={handleChangeQuery}
+            variant="outlined"
+            placeholder="Search..."
             size="small"
-            disabled={!searchParams.query}
-            onChange={(e) => {
-              setSearchParams({...searchParams, color: (e.target.value as Color) || undefined});
-            }}
-          >
-            <MenuItem value={Color.BLACK_AND_WHITE}>BLACK_AND_WHITE</MenuItem>
-            <MenuItem value={Color.BLACK}>BLACK</MenuItem>
-            <MenuItem value={Color.WHITE}>WHITE</MenuItem>
-            <MenuItem value={Color.YELLOW}>YELLOW</MenuItem>
-            <MenuItem value={Color.ORANGE}>ORANGE</MenuItem>
-            <MenuItem value={Color.RED}>RED</MenuItem>
-            <MenuItem value={Color.PURPLE}>PURPLE</MenuItem>
-            <MenuItem value={Color.MAGENTA}>MAGENTA</MenuItem>
-            <MenuItem value={Color.GREEN}>GREEN</MenuItem>
-            <MenuItem value={Color.TEAL}>TEAL</MenuItem>
-            <MenuItem value={Color.BLUE}>BLUE</MenuItem>
-            <MenuItem value={0}>-</MenuItem>
-          </Select>
-        </FormControl>
+          />
 
-        <FormControl sx={{minWidth: 120 }}>
-          <InputLabel id="order-label" size='small'>Order</InputLabel>
-          <Select
-            labelId='order-label'
-            value={searchParams.orderBy || ''}
-            label="Order"
-            size="small"
-            disabled={!searchParams.query}
-            onChange={(e) => {
-              console.log('order changed to: ', e.target.value)
-              setSearchParams({...searchParams, orderBy: (e.target.value as OrderBy) || undefined});
-            }}
-          >
-            <MenuItem value={OrderBy.LATEST}>LATEST</MenuItem>
-            <MenuItem value={OrderBy.RELEVANT}>RELEVANT</MenuItem>
-            <MenuItem value={0}>-</MenuItem>
-          </Select>
-        </FormControl>
+          <FormControl sx={{minWidth: 120 }}>
+            <InputLabel id="color-label" size='small'>Color</InputLabel>
+            <Select
+              labelId='color-label'
+              value={searchParams.color || ''}
+              label="Color"
+              size="small"
+              disabled={!searchParams.query}
+              onChange={(e) => {
+                setSearchParams({...searchParams, color: (e.target.value as Color) || undefined});
+              }}
+            >
+              <MenuItem value={Color.BLACK_AND_WHITE}>BLACK_AND_WHITE</MenuItem>
+              <MenuItem value={Color.BLACK}>BLACK</MenuItem>
+              <MenuItem value={Color.WHITE}>WHITE</MenuItem>
+              <MenuItem value={Color.YELLOW}>YELLOW</MenuItem>
+              <MenuItem value={Color.ORANGE}>ORANGE</MenuItem>
+              <MenuItem value={Color.RED}>RED</MenuItem>
+              <MenuItem value={Color.PURPLE}>PURPLE</MenuItem>
+              <MenuItem value={Color.MAGENTA}>MAGENTA</MenuItem>
+              <MenuItem value={Color.GREEN}>GREEN</MenuItem>
+              <MenuItem value={Color.TEAL}>TEAL</MenuItem>
+              <MenuItem value={Color.BLUE}>BLUE</MenuItem>
+              <MenuItem value={0}>-</MenuItem>
+            </Select>
+          </FormControl>
 
-        <FormControl sx={{minWidth: 120 }}>
-          <InputLabel id="orientation-label" size='small'>Orientation</InputLabel>
-          <Select
-            labelId='orientation-label'
-            value={searchParams.orientation || ''}
-            label="Orientation"
-            size="small"
-            disabled={!searchParams.query}
-            onChange={(e) => {
-              setSearchParams({...searchParams, orientation: (e.target.value as Orientation) || undefined});
-            }}
-          >
-            <MenuItem value={Orientation.LANDSCAPE}>LANDSCAPE</MenuItem>
-            <MenuItem value={Orientation.PORTRAIT}>PORTRAIT</MenuItem>
-            <MenuItem value={Orientation.SQUARE}>SQUARE</MenuItem>
-            <MenuItem value={0}>-</MenuItem>
-          </Select>
-        </FormControl>
+          <FormControl sx={{minWidth: 120 }}>
+            <InputLabel id="order-label" size='small'>Order</InputLabel>
+            <Select
+              labelId='order-label'
+              value={searchParams.orderBy || ''}
+              label="Order"
+              size="small"
+              disabled={!searchParams.query}
+              onChange={(e) => {
+                console.log('order changed to: ', e.target.value)
+                setSearchParams({...searchParams, orderBy: (e.target.value as OrderBy) || undefined});
+              }}
+            >
+              <MenuItem value={OrderBy.LATEST}>LATEST</MenuItem>
+              <MenuItem value={OrderBy.RELEVANT}>RELEVANT</MenuItem>
+              <MenuItem value={0}>-</MenuItem>
+            </Select>
+          </FormControl>
 
-        <IconButton
-          onClick={handleSearch}
-        >
-          <SearchIcon/>
-        </IconButton>
-        <IconButton
-          onClick={handleClearSearch}
-        >
-          <ClearIcon/>
-        </IconButton>
-      </div>
-        <ImageList variant="masonry" cols={7} gap={8}>
-          {images.map((item) => (
-            <ImageListItem key={item.id}>
-              <img
-                src={`${item.urls.small}`}
-                srcSet={`${item.urls.small}`}
-                alt={item.alt}
-                loading="lazy"
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
+          <FormControl sx={{minWidth: 120 }}>
+            <InputLabel id="orientation-label" size='small'>Orientation</InputLabel>
+            <Select
+              labelId='orientation-label'
+              value={searchParams.orientation || ''}
+              label="Orientation"
+              size="small"
+              disabled={!searchParams.query}
+              onChange={(e) => {
+                setSearchParams({...searchParams, orientation: (e.target.value as Orientation) || undefined});
+              }}
+            >
+              <MenuItem value={Orientation.LANDSCAPE}>LANDSCAPE</MenuItem>
+              <MenuItem value={Orientation.PORTRAIT}>PORTRAIT</MenuItem>
+              <MenuItem value={Orientation.SQUARE}>SQUARE</MenuItem>
+              <MenuItem value={0}>-</MenuItem>
+            </Select>
+          </FormControl>
+
+          <IconButton
+            onClick={handleSearch}
+          >
+            <SearchIcon/>
+          </IconButton>
+          <IconButton
+            onClick={handleClearSearch}
+          >
+            <ClearIcon/>
+          </IconButton>
+        </div>
+        <ImagesList />
       </Container>
     </>
   );
---
+++ src/features/images/components/ImagesList.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { Modal, Link, Box, ImageListItem, ImageList } from '@mui/material';
+import { useAppSelector } from '../../../app/hooks';
+import {
+  selectImages
+} from '../imagesSlice';
+import { Image } from '../types';
+
+const boxStyle = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  textAlign: 'center',
+};
+
+const imageContainerStyle: React.CSSProperties = {
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+export function ImagesList() {
+  const images = useAppSelector(selectImages);
+  const [selectedImage, setSelectedImage] = useState<Image | undefined>();
+
+  const handleClose = () => setSelectedImage(undefined);
+
+  return (
+    <>
+        <ImageList variant="masonry" cols={7} gap={8}>
+          {images.map((item) => (
+            <ImageListItem
+              key={item.id}
+              onClick={() => {
+                console.log('image click')
+                setSelectedImage(item)
+              }}
+            >
+                <img
+                  src={`${item.urls.small}`}
+                  srcSet={`${item.urls.small}`}
+                  alt={item.alt}
+                  loading="lazy"
+                  onClick={() => {
+                    setSelectedImage(item);
+                  }}
+                />
+            </ImageListItem>
+          ))}
+        </ImageList>
+        <Modal
+          open={Boolean(selectedImage)}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={boxStyle}>
+            <div style={imageContainerStyle}>
+              <img
+                style={{maxWidth: '100%', maxHeight: 500}}
+                src={`${selectedImage?.urls.regular}`}
+                alt={selectedImage?.alt}
+              />
+            </div>
+            <Link href={selectedImage?.urls.full} target="_blank"> Open in a new tab</Link>
+          </Box>
+        </Modal>
+    </>
+  );
+}
